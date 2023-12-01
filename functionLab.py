6ab2080d1f7180e19a02b7351e6b8b93a16b1855
DRoberts487 <146487048+DRoberts487@users.noreply.github.com>
2023-12-01 15:53:00 -0600

    Add files via upload

---
+++ functionLab.py
@@ -0,0 +1,19 @@
+def print_collection(label, items):
+    print (label, items)
+movie = 'Movies:'
+movieList = ["John Wick","John Wick 2", "John Wick 3"]
+
+
+car = 'Cars'
+carList = ["Ford", "Chevy", "Buick"]
+
+
+print_collection(movie,movieList)
+print (len(movieList))
+print_collection(car,carList)
+print (len(carList))
+
+
+
+
+
